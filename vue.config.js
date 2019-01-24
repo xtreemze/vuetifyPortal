0d27e1292eb48aee48ff31561eebd78774f1e449
Carlos Velasco <carlos.velasco@teleservice.net>
2019-01-24 19:59:36 +0100

    base URL

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,18 @@ module.exports = {
 	publicPath: process.env.NODE_ENV === 'production'
 		? '/vuetifyPortal/'
 		: '/',
+
 	lintOnSave: true,
 
 	pwa: {
 		name: "Customer portal"
-	}
+	},
+
+	baseUrl: '/vuetifyPortal/',
+	outputDir: undefined,
+	assetsDir: undefined,
+	runtimeCompiler: undefined,
+	productionSourceMap: undefined,
+	parallel: undefined,
+	css: undefined
 };
